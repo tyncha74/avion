b3bda9fef0246b529b06a979d713e586ac58c4d3
tyncha74 <tyn4tyk74@gmail.com>
2024-07-10 21:16:36 +0600

    two

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,9 +3,19 @@ import { CiSearch } from "react-icons/ci";
 import { SlBasket } from "react-icons/sl";
 import { FaRegUser } from "react-icons/fa";
 import { Link, NavLink } from 'react-router-dom'
+import {useState} from 'react'
+import { GiHamburgerMenu } from "react-icons/gi";
 
 
 export default function Header() {
+
+
+    const[menuActive, setMenuActive] = useState (false)
+
+    const toggleMenu =()=>{
+        setMenuActive(! menuActive)
+    }
+
     return (
         <div>
             <div className='header-top'>
@@ -14,11 +24,12 @@ export default function Header() {
                 <div className='header-icons'>
                     <SlBasket />
                     <FaRegUser />
+                    < GiHamburgerMenu  onClick ={ toggleMenu} className='burger-menu'/>
                     
                 </div>
 
             </div>
-            <div className='header-bottom'>
+            <div className={`header-bottom ${menuActive ? 'active' : ''}`}>
                 <NavLink to="/" className='Link'> Glavnaya</NavLink>
                 <NavLink  to="page-two" className='Link'>Glavnaya2</NavLink>
                 <NavLink to="product-listing" className='Link'>Katalog</NavLink>
--- src/pages/home-page2/home-page2.jsx
+++ src/pages/home-page2/home-page2.jsx
@@ -1,9 +1,70 @@
 
 
+import { IoIosCheckmarkCircleOutline } from 'react-icons/io';
 import './home-page2.css'
+import {useState} from 'react'
+import { IoCloseOutline } from "react-icons/io5";
+import { LuSprout } from 'react-icons/lu';
+import { MdOutlineFeaturedPlayList } from 'react-icons/md';
+import { TbTruckDelivery } from 'react-icons/tb';
+
+
+
+
+
 export default function HomePage2 ()
 {
+    const [modal, setModal]= useState(false)
+    const toggleModal =()=>{
+        setModal(!modal)
+    }
+
     return(
-        <div className="home-page2"> stranisa 2</div>
+        <div> 
+       <div className="home-page2"> 
+       <button onClick={toggleModal} className='modal-btn'> otkryt okno</button>
+        {
+            modal ? (
+                <div className='conteiner-modal' onClick={toggleModal}> 
+                <div className='modal'>
+                    <IoCloseOutline onClick={toggleModal} size={30} className='close-btn'/>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, nesciunt!
+                </div>
+                
+                </div>
+
+            ) : ''
+        }</div>
+         <div className='two'>
+                <h1>What makes our brand different</h1>
+
+                <div className='section-two'>
+                    <div  style={{backgroundColor:"#f9f9f9", padding:"30px"}} className='block-one'>
+                        <TbTruckDelivery size={30} />
+                        <h2>Next day as standard</h2>
+                        <p>Order before 3pm and get your order
+                            the next day as standard</p>
+                    </div>
+                    <div  style={{backgroundColor:"#f9f9f9", padding:"30px"}} className='block-one'>
+                        <IoIosCheckmarkCircleOutline size={30} />
+                        <h2>Made by true artisans</h2>
+                        <p>Handmade crafted goods made with
+                            real passion and craftmanship</p>
+                    </div>
+                    <div  style={{backgroundColor:"#f9f9f9", padding:"30px"}} className='block-one'>
+                        <MdOutlineFeaturedPlayList size={30} />
+                        <h2>Unbeatable prices</h2>
+                        <p>For our materials and quality you won’t find better prices anywhere</p>
+                    </div>
+                    <div  style={{backgroundColor:"#f9f9f9", padding:"30px"}} className='block-one'>
+                        <LuSprout size={30} />
+                        <h2>Recycled packaging</h2>
+                        <p>We use 100% recycled packaging to ensure our footprint is manageable</p>
+                    </div>
+                </div>
+
+        </div>
+        </div>
+
     )
 }
