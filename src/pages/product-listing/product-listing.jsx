da2377b5a896bcf77834f1e3a8032c5004f63c87
tyncha74 <tyn4tyk74@gmail.com>
2024-07-15 21:31:05 +0600

    five

--- src/pages/product-listing/product-listing.jsx
+++ src/pages/product-listing/product-listing.jsx
@@ -1,19 +1,44 @@
 import './product-listing.css'
 import catalog from '../../img/catalog/catalog.png'
-import {useState} from 'react'
+import { useState } from 'react'
+import img1 from '../../img/card/img1.png'
+import img2 from '../../img/card/img2.png'
+import img3 from '../../img/card/img3.png'
+import img4 from '../../img/card/img4.png'
+import Card from '../../components/card/card';
+
 
 export default function ProductListing() {
 
-    const [counter, setCounter]=useState(1)
-    function increment()
-    {
-        setCounter(counter+1)
+    const [counter, setCounter] = useState(1)
+    const [inputValues, setInputValues]=useState('')
+    const [data,setData]=useState({
+        massage:"",
+        user:""
+    })
+    console.log(data);
+
+    function saveData(){
+        setData({
+            massage:inputValues,
+            user:"mark@gmail.com"
+        })
     }
-    function decrement(){
-        if (counter>1){
-            setCounter(counter-1)
+    function increment() {
+        setCounter(counter + 1)
+    }
+    function decrement() {
+        if (counter > 1) {
+            setCounter(counter - 1)
         }
     }
+    const allCards = [
+        { name: "The Dandy chair", price: 250, img: img1 },
+        { name: "Rustic Vase Set", price: 155, img: img2 },
+        { name: "The Silky Vase", price: 125, img: img3 },
+        { name: "The Lucy Lamp", price: 399, img: img4 },
+    ]
+    
 
     return (
         <div className="product-listing">
@@ -65,6 +90,29 @@ export default function ProductListing() {
                         </div>
 
                     </div>
+
+                </div>
+            </div>
+            <div className='section-three'>
+                <h1>New ceramics</h1>
+                <div className='conteiner'>
+                    {
+                        allCards.map(item => <Card data={item} />)
+                    }
+
+
+                </div>
+                <div className='button'><button>View collection</button></div>
+            </div>
+            <div className='last-block' style={{width:'100%',height:'100vh'}}>
+                <div className='conteiner'>
+                    <h1>Join the club and get the benefits</h1>
+                    <p>Sign up for our newsletter and receive exclusive
+                     offers on new <br />ranges, sales, pop up stores and more</p>
+                     <div className='submit-form'>
+                        <input  onChange={(e)=>setInputValues(e.target.value)} type="text"  placeholder='your@email.com'/>
+                        <button onClick={saveData} >Sing up</button>
+                     </div>
 
                 </div>
             </div>
